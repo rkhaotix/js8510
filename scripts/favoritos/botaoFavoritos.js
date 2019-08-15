fcf7692654b82f0d6db4228eb810bb4328ca1b99
Raphael A. Silva <rkhaotix@gmail.com>
2019-08-15 16:46:04 -0300

    evitando salvar favorito se o usuário nao aceitou salvar infos

--- scripts/favoritos/botaoFavoritos.js
+++ scripts/favoritos/botaoFavoritos.js
@@ -1,10 +1,17 @@
 import * as favoritos from "/scripts/storage/favoritos.js";
+import { aceitouSalvar } from "/scripts/storage/aceitouSalvar.js";
+
 
 for(const fav of favoritos.listaFavoritos) {
     $Cake.addFavorite(fav);
 }
 
-$btnFavoritos.addEventListener('click', function(){
+$btnFavoritos.addEventListener('click', function(){ 
+
+    if(!aceitouSalvar) {
+        return;
+    }
+
     const endereco = $iframeConteudo.contentWindow.location.href;
     const nomeFavorito = prompt("Nome do favorito?", $inputEndereco.value) || endereco;
 
@@ -12,6 +19,7 @@ $btnFavoritos.addEventListener('click', function(){
         nome: nomeFavorito,
         descricao: endereco
     };
+
 
     favoritos.salvarFavorito(favorito);
     $Cake.addFavorite(favorito);
